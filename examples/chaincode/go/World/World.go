5b72a67857d2ebcd1faf51972cce8c672fe4a8ed
Rishabh Sanghi <rishabhsanghi@Rishabhs-MacBook-Pro.local>
2016-07-08 01:37:44 -0400

    new changes

--- examples/chaincode/go/World/World.go
+++ examples/chaincode/go/World/World.go
@@ -1,26 +1,4 @@
-/*
-Copyright IBM Corp. 2016 All Rights Reserved.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-		 http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package main
-
-//WARNING - this chaincode's ID is hard-coded in chaincode_example04 to illustrate one way of
-//calling chaincode from a chaincode. If this example is modified, chaincode_example04.go has
-//to be modified as well with the new ID of chaincode_example02.
-//chaincode_example05 show's how chaincode ID can be passed in as a parameter instead of
-//hard-coding.
 
 import (
 	"errors"
@@ -36,7 +14,7 @@ type SimpleChaincode struct {
 }
 
 type Marble struct{
-	Name string `json:"name"`					//the fieldtags are needed to keep case from bouncing around
+	Name string `json:"name"`					
 	Color string `json:"color"`
 	Size int `json:"size"`
 	User string `json:"user"`
@@ -105,10 +83,9 @@ func (t *SimpleChaincode) Init(stub *shim.ChaincodeStub, function string, args [
 	return nil, nil
 }
 
-//Entry Point
-func (t *SimpleChaincode) Run(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
-	//fmt.Println("run is running " + function)
 
+func (t *SimpleChaincode) Run(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
+	
 	// Handle different functions
 	if function == "init" {											
 		return t.Init(stub, "init", args)
@@ -133,7 +110,6 @@ func (t *SimpleChaincode) Run(stub *shim.ChaincodeStub, function string, args []
 	} else if function == "redeem" {									
 		//return t.buy(stub, args)
 	}
-	fmt.Println("run did not find func: " + function)						//error
 
 	return nil, errors.New("Received unknown function invocation")
 }
